93d6ba271adb245fa4dd8f0de83a0476833ae24e
deivisjl <deivisjl@gmail.com>
2021-11-23 16:38:07 -0600

    Crud de preliminar de books

--- LumenApiGateway/app/Http/Controllers/BookController.php
+++ LumenApiGateway/app/Http/Controllers/BookController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Traits\ApiResponser;
 use Illuminate\Http\Request;
 use App\Services\BookService;
+use Illuminate\Http\Response;
 
 class BookController extends Controller
 {
@@ -23,22 +24,22 @@ class BookController extends Controller
 
     public function index()
     {
-
+        return $this->successResponse($this->bookService->obtainBooks());
     }
     public function store(Request $request)
     {
-
-    }
+        return $this->successResponse($this->bookService->createBooks($request->all()), Response::HTTP_CREATED);
+    }   
     public function show($book)
     {
-
+        return $this->successResponse($this->bookService->obtainBook($book), Response::HTTP_FOUND);
     }
     public function update(Request $request, $book)
     {
-
+        return $this->successResponse($this->bookService->editBook($request->all(),$book), Response::HTTP_FOUND);
     }
     public function destroy($book)
     {
-        
+        return $this->successResponse($this->bookService->deleteBook($book));
     }
 }
