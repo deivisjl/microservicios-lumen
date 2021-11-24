010dc180c0b0ccc29a0e46f2213afc091be30695
deivisjl <deivisjl@gmail.com>
2021-11-24 17:06:39 -0600

    Mantenimiento de usuarios

--- LumenApiGateway/app/Traits/ApiResponser.php
+++ LumenApiGateway/app/Traits/ApiResponser.php
@@ -10,6 +10,11 @@ trait ApiResponser
         return response($data,$code)->header('Content-Type','application/json');
     }
 
+    public function validResponse($data, $code = Response::HTTP_OK)
+    {
+        return response()->json(['data' => $data], $code);
+    }
+
     public function errorResponse($message, $code)
     {
         return response()->json(['error' => $message, 'code' => $code], $code);
